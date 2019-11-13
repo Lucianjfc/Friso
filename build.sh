85f7367b4ca8d8224efe7b87310c30fd69e8cdf4
119110985 LUCIAN JULIO FELIX DA COSTA <lucian.costa@ccc.ufcg.edu.br>
2019-11-13 13:09:36 -0300

    Adicionado run.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,5 @@
 #comentários
 echo "Instalando configurações de ambiante!"
 apt-get install python-pip
-pip install numpy
 pip install matplotlib
 echo "Configurações instaladas"
---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+IMAGE_PATH=$1
+DIR=$2
+python run.py $IMAGE_PATH $DIR 
